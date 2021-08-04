7097bd24327ec09a56077dc4838d091abb72b08a
zhangjian <zhangxjian@me.com>
2021-08-04 15:14:15 +0800

    第 5 章 自動佈局介紹 #1

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -9,32 +9,6 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  @IBAction func showMessage(sender: UIButton) {
-    
-    
-    let emojiDict: [String: String] = [
-      "👾": "异形怪兽",
-      "👻": "幽灵",
-      "🤓": "戴眼镜的笑脸",
-      "🤖": "机器人脸"
-    ]
-    
-    let selectedButton = sender
-    
-    if let wordToLookup = selectedButton.titleLabel?.text {
-      
-      let meaning = emojiDict[wordToLookup]
-      
-      let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
-      
-      alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-      
-      present(alertController, animated: true, completion: nil)
-      
-    }
-    
-  }
-  
   
   override func viewDidLoad() {
     super.viewDidLoad()
