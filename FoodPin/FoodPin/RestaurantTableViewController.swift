873704cd801b6b639991e9eb74f4ea283fca44a8
zhangjian <zhangxjian@me.com>
2021-08-06 10:34:19 +0800

    第 9 章 使用 Prototype Cell、差異性資料源與深色模式自訂表格視圖 #1

--- FoodPin/FoodPin/RestaurantTableViewController.swift
+++ FoodPin/FoodPin/RestaurantTableViewController.swift
@@ -11,6 +11,12 @@ class RestaurantTableViewController: UITableViewController {
   
   var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
   
+  var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
+  
+  var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+
+  var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+  
   lazy var dataSource = configureDataSource()
   
   override func viewDidLoad() {
@@ -102,10 +108,12 @@ class RestaurantTableViewController: UITableViewController {
     let dataSource = UITableViewDiffableDataSource<Section, String>(
       tableView: tableView,
       cellProvider: {  tableView, indexPath, restaurantName in
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
         
-        cell.textLabel?.text = restaurantName
-        cell.imageView?.image = UIImage(named: "restaurant")
+        cell.nameLabel.text = restaurantName
+        cell.thumbnailImageView.image = UIImage(named: self.restaurantImages[indexPath.row])
+        cell.locationLabel.text = self.restaurantLocations[indexPath.row]
+        cell.typeLabel.text = self.restaurantTypes[indexPath.row]
         
         return cell
       }
