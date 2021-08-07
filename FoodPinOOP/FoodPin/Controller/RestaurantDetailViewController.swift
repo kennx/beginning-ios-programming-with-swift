a7f380d0acc51dc8e2f8a512fcab3dc6081963e9
zhangjian <zhangxjian@me.com>
2021-08-07 14:53:53 +0800

    第 13 章 導覽控制器與 Segue

---
+++ FoodPinOOP/FoodPin/Controller/RestaurantDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by 张剑 on 2021/8/7.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController {
+  
+  @IBOutlet var restaurantImageView: UIImageView!
+  
+  var restaurantImageName = ""
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    navigationItem.largeTitleDisplayMode = .never
+    restaurantImageView.image = UIImage(named: restaurantImageName)
+    // Do any additional setup after loading the view.
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
