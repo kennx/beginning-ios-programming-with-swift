d8605b401fe073aff7c8033ce99d05751bc12e2e
zhangjian <zhangxjian@me.com>
2021-08-09 17:54:42 +0800

    update

---
+++ FoodPinDetailView/FoodPin/View/RestaurantDetailTextCell.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantDetailTextCell.swift
+//  FoodPin
+//
+//  Created by 张剑 on 2021/8/9.
+//
+
+import UIKit
+
+class RestaurantDetailTextCell: UITableViewCell {
+  
+  @IBOutlet var descriptionLabel: UILabel! {
+    didSet {
+      descriptionLabel.numberOfLines = 0
+    }
+  }
+  
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
