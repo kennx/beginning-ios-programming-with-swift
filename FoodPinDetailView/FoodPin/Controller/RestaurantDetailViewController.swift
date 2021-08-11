4462c2abebe9a9dab571475fdcdc00e94cd99b2c
zhangjian <zhangxjian@me.com>
2021-08-11 11:52:45 +0800

    第 15 章 自訂導覽列、深色模式與動態型態

--- FoodPinDetailView/FoodPin/Controller/RestaurantDetailViewController.swift
+++ FoodPinDetailView/FoodPin/Controller/RestaurantDetailViewController.swift
@@ -8,69 +8,80 @@
 import UIKit
 
 class RestaurantDetailViewController: UIViewController {
-
-    @IBOutlet var tableView: UITableView!
-    @IBOutlet var headerView: RestaurantDetailHeaderView!
+  
+  @IBOutlet var tableView: UITableView!
+  @IBOutlet var headerView: RestaurantDetailHeaderView!
+  
+  var restaurant: Restaurant = Restaurant()
+  
+  override func viewDidLoad() {
+    tableView.contentInsetAdjustmentBehavior = .never
+    super.viewDidLoad()
     
-    var restaurant: Restaurant = Restaurant()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-      
-      
-        
-        navigationController?.navigationBar.prefersLargeTitles = false
-
-        // Configure header view
-        headerView.nameLabel.text = restaurant.name
-        headerView.typeLabel.text = restaurant.type
-        headerView.headerImageView.image = UIImage(named: restaurant.image)
-        
-        let heartImage = restaurant.isFavorite ? "heart.fill" : "heart"
-        headerView.heartButton.tintColor = restaurant.isFavorite ? .systemYellow : .white
-        headerView.heartButton.setImage(UIImage(systemName: heartImage), for: .normal)
-
-        // Configure the data source
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.separatorStyle = .none
-      
-      tableView.contentInsetAdjustmentBehavior = .never
-    }
     
-
+    
+    navigationController?.navigationBar.prefersLargeTitles = false
+    
+    // Configure header view
+    headerView.nameLabel.text = restaurant.name
+    headerView.typeLabel.text = restaurant.type
+    headerView.headerImageView.image = UIImage(named: restaurant.image)
+    
+    let heartImage = restaurant.isFavorite ? "heart.fill" : "heart"
+    headerView.heartButton.tintColor = restaurant.isFavorite ? .systemYellow : .white
+    headerView.heartButton.setImage(UIImage(systemName: heartImage), for: .normal)
+    
+    // Configure the data source
+    tableView.delegate = self
+    tableView.dataSource = self
+    tableView.separatorStyle = .none
+    
+    
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.hidesBarsOnSwipe = false
+    navigationController?.setNavigationBarHidden(false, animated: true)
+  }
+  
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+  
+  
 }
 
 extension RestaurantDetailViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 2
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    switch indexPath.row {
+    case 0:
+      let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
+      
+      cell.descriptionLabel.text = restaurant.description
+      
+      return cell
+      
+    case 1:
+      let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTwoColumnCell.self), for: indexPath) as! RestaurantDetailTwoColumnCell
+      
+      cell.column1TitleLabel.text = "Address"
+      cell.column1TextLabel.text = restaurant.location
+      cell.column2TitleLabel.text = "Phone"
+      cell.column2TextLabel.text = restaurant.phone
+      
+      return cell
+      
+    default:
+      fatalError("Failed to instantiate the table view cell for detail view controller")
+      
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch indexPath.row {
-        case 0:
-            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTextCell.self), for: indexPath) as! RestaurantDetailTextCell
-            
-            cell.descriptionLabel.text = restaurant.description
-            
-            return cell
-            
-        case 1:
-            let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: RestaurantDetailTwoColumnCell.self), for: indexPath) as! RestaurantDetailTwoColumnCell
-            
-            cell.column1TitleLabel.text = "Address"
-            cell.column1TextLabel.text = restaurant.location
-            cell.column2TitleLabel.text = "Phone"
-            cell.column2TextLabel.text = restaurant.phone
-            
-            return cell
-            
-        default:
-            fatalError("Failed to instantiate the table view cell for detail view controller")
-            
-        }
-    }
-
+  }
+  
 }
