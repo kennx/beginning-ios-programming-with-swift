258e7f9c649fb10d47ae439e0c573e4c56573545
zhangjian <zhangxjian@me.com>
2021-08-13 08:56:51 +0800

    no message

--- FoodPinStaticTableStarter/FoodPin/Controller/MapViewController.swift
+++ FoodPinStaticTableStarter/FoodPin/Controller/MapViewController.swift
@@ -2,113 +2,79 @@
 //  MapViewController.swift
 //  FoodPin
 //
-//  Created by 张剑 on 2021/8/11.
+//  Created by Simon Ng on 30/10/2020.
 //
 
 import UIKit
 import MapKit
 
 class MapViewController: UIViewController {
-  
-  @IBOutlet var mapView: MKMapView!
-<<<<<<< HEAD
-=======
-  
->>>>>>> 9425ffe... update
-  var restaurant: Restaurant = Restaurant()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    mapView.delegate = self
-<<<<<<< HEAD
-    
-    mapView.showsScale = true
-    mapView.showsTraffic = true
-    mapView.showsCompass = true
-    
-    let geoCoder = CLGeocoder()
-    
-    geoCoder.geocodeAddressString(restaurant.location) { placemarks, error in
-=======
-    mapView.showsCompass = true
-    mapView.showsScale = true
-    mapView.showsTraffic = true
-    
-    let geoCoder = CLGeocoder()
-    
-    geoCoder.geocodeAddressString(restaurant.location) { (placemarks, error) in
->>>>>>> 9425ffe... update
-      if let error = error {
-        print(error.localizedDescription)
-        return
-      }
-      
-      if let placemarks = placemarks {
-        let placemark = placemarks[0]
+
+    @IBOutlet var mapView: MKMapView!
         
-        let annotation = MKPointAnnotation()
-        annotation.title = self.restaurant.name
-        annotation.subtitle = self.restaurant.type
+    var restaurant = Restaurant()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        if let location = placemark.location {
-          annotation.coordinate = location.coordinate
-          
-          self.mapView.showAnnotations([annotation], animated: true)
-          self.mapView.selectAnnotation(annotation, animated: true)
-        }
-      }
+        // Set the delegate
+        mapView.delegate = self
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        mapView.showsTraffic = true
+        
+        // Convert address to coordinate and annotate it on map
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(restaurant.location, completionHandler: { placemarks, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let placemarks = placemarks {
+                // Get the first placemark
+                let placemark = placemarks[0]
+                
+                // Add annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = self.restaurant.name
+                annotation.subtitle = self.restaurant.type
+                
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    
+                    // Display the annotation
+                    self.mapView.showAnnotations([annotation], animated: true)
+                    self.mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+            
+        })
     }
-    
-    // Do any additional setup after loading the view.
-  }
-  
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
+
+
 }
 
-
 extension MapViewController: MKMapViewDelegate {
-<<<<<<< HEAD
-  
-  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-    let identifier = "MyMarker"
     
-=======
-  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-    let identifier = "MyMarker"
->>>>>>> 9425ffe... update
-    if annotation.isKind(of: MKUserLocation.self) {
-      return nil
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyMarker"
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            return nil
+        }
+        
+        // Reuse the annotation if possible
+        var annotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        
+        annotationView?.glyphText = "😋"
+//        annotationView?.glyphImage = UIImage(systemName: "arrowtriangle.down.circle")
+        annotationView?.markerTintColor = UIColor.orange
+        
+        return annotationView
     }
-    
-    var annotationView: MKMarkerAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView
-    
-    if annotationView == nil {
-      annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-    }
-<<<<<<< HEAD
-    
-    annotationView?.glyphText = "😋"
-    // annotationView?.glyphImage = UIImage(systemName: "arrowtriangle.down.circle")
-    annotationView?.markerTintColor = UIColor.orange
-    
-    return annotationView
-  }
-  
-=======
-    annotationView?.glyphImage = UIImage(systemName: "arrowtriangle.down.circle")
-    annotationView?.markerTintColor = UIColor.orange
-    return annotationView
-  }
->>>>>>> 9425ffe... update
 }
